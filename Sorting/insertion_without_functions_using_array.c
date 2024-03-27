9435e947b1c21f26e57e0eda8c765afe6fa23911
Harshdeep Singh <94488766+harshdeepsinghin@users.noreply.github.com>
2024-03-27 11:46:29 +0530

    selection sort without functions using array done

--- Sorting/insertion_without_functions_using_array.c
+++ Sorting/insertion_without_functions_using_array.c
@@ -2,18 +2,26 @@
 
 int main (void)
 {
-    int SIZE = 6
-    int A[SIZE] = {4,1,3,9,45,23};
+    int size = 6;
+    int A[size] = {4,1,3,9,45,23};
 
     for (int i=1; i<SIZE; i++)
     {
         if (A[i-1]>A[i])
         {
             int key = A[i];
-            A[i] = A[i-1];
-            A[i] = key;
+            // Shifting
+            for (int j=i; j>sorted; j--)
+            {
+                A[j]=A[j-1];
+            }
+            A[sorted] = key;
         }
     }
     
+    for (int i=0; i<size; i++)
+    {
+        printf("%d ", A[i]);
+    }
     return 0;
 }
