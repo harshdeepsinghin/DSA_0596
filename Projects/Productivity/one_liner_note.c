df261a90ed788445a618cc6b110b63c9bdf73cf6
Harshdeep Singh <94488766+harshdeepsinghin@users.noreply.github.com>
2024-04-09 18:13:00 +0530

    one liner note command line argument

--- Projects/Productivity/one_liner_note.c
+++ Projects/Productivity/one_liner_note.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 // typedef
 
 typedef char *string;
@@ -8,8 +8,11 @@ typedef char *string;
 
 string DATA_FILE = "data/one_liner_notes.csv";
 
+// Functions declaration
 
-int main (int argc, char* argv[])
+void help(string argv[]);
+
+int main (int argc, char *argv[])
 {
     FILE *checker, *fptr;   // FILE pointers declaration
 
@@ -23,32 +26,29 @@ int main (int argc, char* argv[])
         fputs(attributes, fptr);
         fclose(fptr);
     }
-    printf("You have entered %d arguments.\n", argc);
     
-    while (argc > 1)
+    if ((argc == 1) || (argv[1][0] == '-'))
     {
-        while (argv[1][0] == '-')
+        help(argv);
+    }  
+    else
+    {
+        for (int i = 1; i < argc; i++)
         {
-            switch (argv[1][1])
-            {
-                case 'a':
-                    printf("a is %s\n", argv[2]);
-                    break;
-                case 'b':
-                    printf("b is %s\n", argv[2]);
-                    break;
-                default:
-                    printf("default\n");
-                    break;
-            }
-            argv++;
-            argc--;
+            if (i == argc - 1)
+                printf("%s", argv[i]);
+            else
+                printf("%s ", argv[i]);
         }
-        argv++;
-        argc--;
     }
     
+    
     return 0;
+}
+
+void help(char *argv[])
+{
+    printf("Usage:\n\t%s I love penguins\n\nBasic options:\n--help, -h\t\tPrints this help guide.", argv[0]);
 }
 
 
