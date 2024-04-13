fb4bbf9769a561343901a597912b8c5ba9a77bf9
Harshdeep Singh <94488766+harshdeepsinghin@users.noreply.github.com>
2024-04-13 14:33:14 +0530

    25 Questions - 15 print reverse array done

--- Assignments/25_Questions/15_print_reverse_array.c
+++ Assignments/25_Questions/15_print_reverse_array.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+
+/* Program to print elements of an array in reverse order: */
+
+int main (void)
+{
+    const int N = 5;    // Array size
+    int A[N] = {2,4,6,8,10};    // Array initialization
+
+    // Original Array Traversing
+    printf("Original Array: ");
+    for (int i=0; i<N; i++)
+    {
+        printf("%d ", A[i]);
+    }
+
+    // Reversed Array Traversing
+    printf("\nReversed Array: ");
+    for (int i=N-1; i>=0; i--)
+    {
+        printf("%d ", A[i]);
+    }
+    
+    return 0;
+}
